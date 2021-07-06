782287f7f9fc45ed4e9f9d1b4f6d45fa3364f69f
rwagner <rwagner000@gmail.com>
2021-07-06 14:58:13 +0200

    debugging transforms

--- train_ce.py
+++ train_ce.py
@@ -204,7 +204,7 @@ def set_loader(opt):
                                         transform=val_transform)
     elif opt.dataset == 'path':
         train_dataset = datasets.ImageFolder(root=opt.data_folder + '/train/',
-                                             transform=trein_transform)
+                                             transform=train_transform)
         val_dataset = datasets.ImageFolder(root=opt.data_folder + '/val_easy/',
                                            transform=val_transform)
     else:
